7ddad67944dec509483aef1c264bf946838e7a8b
indrojit22996 <indrojit22996bhaduri@gmail.com>
2023-03-23 21:16:33 +0530

    Sharpener DOM 01

---
+++ dom.js
@@ -0,0 +1,6 @@
+let header = document.getElementById("main-header");
+header.style.borderBottom = "4px solid #000";
+let headerTit = document.querySelector("#header-title");
+headerTit.innerText = "ADD ITEM";
+headerTit.style.color = "#7AA874";
+headerTit.style.fontWeight = "bold";
